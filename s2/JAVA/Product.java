218a4069a08698a42ca18bd725b56c9a42a997ff
rishal79 <rishalp79@gmail.com>
2025-03-03 23:23:38 +0530

    program 1

---
+++ s2/JAVA/Product.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Product{
+    int pcode,price;
+    String pname;
+    Product(){
+        Scanner sc=new Scanner(System.in);
+        pcode=sc.nextInt();
+        pname=sc.next();
+        price=sc.nextInt();
+    }
+    public static void main(String[] args){
+        Product p1=new Product();
+        Product p2=new Product();
+        Product p3=new Product();
+
+        if(p1.price>p2.price && p1.price>p3.price){
+            System.out.println(p1.pname+" "+p1.price);
+        }else if(p2.price>p3.price){
+            System.out.println(p2.pname+" "+p2.price);
+        }else{
+            System.out.println(p3.pname+" "+p3.price);
+        }
+
+    }
+}

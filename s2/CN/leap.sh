07d7d2b07bcbdaff902347479ee204de44375a9a
RISHAL P <rishalp79@gmail.com>
2025-04-15 12:19:46 +0545

    Add files via upload

---
+++ s2/CN/leap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+read -p "enter the year" year
+if (( year % 400 == 0  ||  ( year % 4 == 0 && year % 100!=0 )))
+then
+	echo "$year is leap year"
+else
+	echo "$year is not leap year"
+fi
